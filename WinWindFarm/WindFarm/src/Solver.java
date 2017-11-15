4a87e0ba7238223ead99594fa5f7972d1c2df225
Lyle-Stevenson <lyleandrewstevenson@gmail.com>
2017-11-15 04:16:46 +0000

    Added basic GA

--- WinWindFarm/WindFarm/src/Solver.java
+++ WinWindFarm/WindFarm/src/Solver.java
@@ -1,3 +1,5 @@
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.util.ArrayList;
 import java.util.Random;
 
@@ -7,7 +9,10 @@ public class Solver {
     boolean[][] individuals;
     double[] fits;
     Random rand;
+    int worstPosition;
+    double maxfit = Double.MIN_VALUE;
     int num_individuals;
+    double mutateRate = 0.08;
     
     ArrayList<double[]> grid;
 
@@ -69,17 +74,90 @@ public class Solver {
 
        /****** evaluate initial population  *************************/
      
-        // this populates the fit[] array with the fitness values for each solution
+        // this populates the fit[] array with the fitness values for each solution 0.0012725460166271383
         evaluate();
 
         /**** PUT YOUR OPTIMISER CODE HERE ***********/
         
-        for (int i=0; i<(10000); i++) {
+        for (int i=0; i<(1000); i++) {
+        	System.out.println(i);
+        	boolean[] parent1 = selection(2);
+        	boolean[] parent2 = selection(2);
 
-        	// add some code to evolve a solution        	
+			boolean[] c1 = crossover(parent1, parent2);
+			
+			c1 = mutation(c1);
+
+			double childFitness = evaluate_individual(c1);
+			
+			if(childFitness > maxfit){
+				individuals[worstPosition] = c1;
+				fits[worstPosition] = childFitness;
+				System.out.println("replacement " + childFitness); 
+			}
+
+        	     	
         }
+        evaluate();
       }
+
+	public boolean[] selection(int tnSize)
+	{	
+			ArrayList<boolean[]> tournament = new ArrayList<boolean[]>();
+			boolean[] chosenParent = null;
+			int pos = rand.nextInt(this.individuals.length-1);
+			double bestFitness = 100;
+			
+			for(int i = 0; i < tnSize; i++)
+			{	
+				tournament.add(this.individuals[pos]);
+			}
+			
+			for(boolean[] indiv : tournament)
+			{
+				if(fits[pos] < bestFitness)
+				{
+					bestFitness = fits[pos];
+					chosenParent = indiv;
+				}
+			}
+			return chosenParent;
+	}
     
+    private boolean[] crossover(boolean[] p1, boolean[] p2) {
+		boolean[] c1 = new boolean[this.grid.size()];
+		
+			int max = this.grid.size() - 1;
+			int min = 1;
+			int crossoverPoint = rand.nextInt(max + min) + min;
+			for(int k = 0; k < p1.length; k++)
+			{
+				 if (k<crossoverPoint){
+				      c1[k] = p1[k];
+				 }else if(k < this.grid.size()){
+				    c1[k]= p2[k];
+				    }
+				 }
+
+		return c1;
+	}
+    
+    private boolean[] mutation(boolean[] child) {
+		
+			for(int i = 0; i < child.length; i++){
+				double mutateChance = new BigDecimal(rand.nextDouble()).setScale(2, RoundingMode.HALF_UP).doubleValue();
+				if(mutateChance <= mutateRate)
+				{
+					if(child[i] == false){
+						child[i] = true;
+					} 
+					else{
+						child[i] = false;
+					}
+				}
+			}
+		return child;
+	}
     
     // evaluate a single chromosome
     private double evaluate_individual(boolean[] child) {
@@ -117,7 +195,7 @@ public class Solver {
         
     }
 
-    // evaluates the whole population
+    // evaluates the whole population 0.0014990008161565244
     private void evaluate() {
         double minfit = Double.MAX_VALUE;
         for (int p=0; p<num_individuals; p++) {
@@ -150,7 +228,10 @@ public class Solver {
             if (fits[p] < minfit) {
                 minfit = fits[p];
             }
-	   
+            if (fits[p] > maxfit) {
+                maxfit = fits[p];
+                worstPosition = p;
+            }
         }
         System.out.println("min " + minfit);
     }
